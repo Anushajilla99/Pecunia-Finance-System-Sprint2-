63eb157612f762776e55fec1cf258749f8060d73
AnushaJilla <16h61a1276@cvsr.ac.in>
2020-05-11 19:57:18 +0530

    remove

--- PecuniaFinanceSystem1/src/main/java/com/capgemini/pecunia/service/LoanRequestService.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.pecunia.service;
-import java.util.ArrayList;
-import com.capgemini.pecunia.bean.LoanRequests;
-public interface LoanRequestService {
-	public String loanRequest(LoanRequests loanreq);
-	public ArrayList<LoanRequests> getAllRequests();
-}

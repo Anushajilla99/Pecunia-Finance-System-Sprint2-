03117ec4450e32398a08f3d9f59933abd2141087
AnushaJilla <16h61a1276@cvsr.ac.in>
2020-05-11 20:29:33 +0530

    remove1

--- PecuniaFinanceSystem1/src/main/java/com/capgemini/pecunia/service/UpdateBalanceService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.pecunia.service;
-
-import com.capgemini.pecunia.bean.LoanDisbursal;
-
-public interface UpdateBalanceService {
-
-	String updateBalance(LoanDisbursal loandis);
-
-}
